1580e479b2cfc888a15686ad299f96eed694ca3e
AleksandraPereira <121905245+AleksandraPereira@users.noreply.github.com>
2023-08-04 21:38:00 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 3333
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+mensagem = client.recv(1024)
+if mensagem == b'SALA':
+    client.send(b'Games')
+    client.send(b'')
+
+    
+    
